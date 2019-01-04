2b51a98ede446e0627c8277c5ac9b387645f628b
Venus Ang <venusang24@gmail.com>
2019-01-04 12:50:27 -0800

    Allow Release Date to be readable

--- src/components/Table/TableSortable.vue
+++ src/components/Table/TableSortable.vue
@@ -5,7 +5,7 @@
         <th v-on:click="sortColumn" scope="col" role="columnheader" id="t-title" :aria-sort="sortSetting">Title</th>
         <th scope="col" role="columnheader" id="t-tagline" aria-label="Tagline" aria-sort="none" class="u-align--left">Tagline</th>
         <th scope="col" role="columnheader" id="t-runtime" aria-label="Runtime" aria-sort="none" class="u-align--left">Runtime</th>
-        <th scope="col" role="columnheader" id="t-release-date" aria-label="Release Date" aria-sort="none" class="u-align--left">Release Date</th>
+        <th scope="col" role="columnheader" id="t-release-date" aria-label="Release Date" aria-sort="none" class="u-align--left">Release<br />Date</th>
         <th scope="col" role="columnheader" id="t-rating" aria-label="Rating" aria-sort="none" class="u-align--left">Rating</th>
         <th scope="col" role="columnheader" id="t-actions" aria-label="Actions" aria-sort="none" class="u-align--left">Actions</th>
       </tr>

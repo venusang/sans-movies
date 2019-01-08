d6cc74d859bbdf65999f9da5dc7ca98ed3b2db1f
Venus Ang <venusang24@gmail.com>
2019-01-07 21:33:32 -0800

    WIP updating TableExpanding but still buggy

--- src/components/Table/TableExpanding.vue
+++ src/components/Table/TableExpanding.vue
@@ -1,33 +1,33 @@
 <template>
   <table class="p-table-expanding p-table--mobile-card" role="grid">
       <thead>
-          <tr role="row">
-              <th scope="col" id="t-title" aria-sort="none">Title</th>
-              <th scope="col" id="t-release-date" aria-sort="none" class="u-align--right">Release Date</th>
-              <th scope="col" id="t-runtime" aria-sort="none" class="u-align--right">Runtime</th>
-              <th scope="col" id="t-actions" aria-sort="none" class="u-align--right">Actions</th>
-              <th class="u-hide"><!-- empty cell required for validation --></th>
-          </tr>
+        <tr role="row">
+          <th scope="col" role="columnheader" id="t-title" aria-sort="none">Title</th>
+          <th scope="col" role="columnheader" id="t-runtime" aria-label="Runtime" aria-sort="none" class="u-align--left">Runtime</th>
+          <th scope="col" role="columnheader" id="t-release-date" aria-label="Release Date" aria-sort="none" class="u-align--left">Release<br />Date</th>
+          <th scope="col" role="columnheader" id="t-rating" aria-label="Rating" aria-sort="none" class="u-align--left">imdb<br />Rating</th>
+          <th scope="col" role="columnheader" id="t-actions" aria-label="Actions" aria-sort="none" class="u-align--left">Actions</th>
+        </tr>
       </thead>
       <tbody>
           <tr v-for="movie in movies" role="row">
-              <td role="rowheader" aria-label="Title">{{movie.original_title}}</td>
-              <td role="gridcell" aria-label="Date" class="u-align--right">{{movie.release_date}}</td>
-              <td role="gridcell" aria-label="Runtime" class="u-align--right">{{movie.runtime}}</td>
+              <td role="rowheader" aria-label="Title">{{movie.Title}}</td>
+              <td role="gridcell" aria-label="Date" class="u-align--right">{{movie.Releases}}</td>
+              <td role="gridcell" aria-label="Runtime" class="u-align--right">{{movie.Runtime}}</td>
               <td role="gridcell" class="u-align--right">
                   <button v-on:click="showMovieDetails" class="u-toggle" :id="`button-${movie.id}`" :aria-controls="`expanded-row-${movie.id}`" aria-expanded="false" data-shown-text="Hide" data-hidden-text="Show">Show</button>
               </td>
               <td :id="`expanded-row-${movie.id}`" class="p-table-expanding__panel" aria-hidden="true">
                   <div class="row">
                       <div class="col-6">
-                          <h3>{{movie.original_title}}</h3>
-                          <p>{{movie.tagline}}</p>
-                          <p>{{movie.runtime}} minutes</p>
-                          <p>{{movie.release_date}}</p>
-                          <p>{{movie.vote_average}}</p>
+                          <h3>{{movie.Title}}</h3>
+                          <p><span class="p-heading--five">Plot:</span> {{movie.Plot}}</p>
+                          <p><span class="p-heading--five">Runtime:</span> {{movie.Runtime}}</p>
+                          <p><span class="p-heading--five">Released:</span> {{movie.Released}}</p>
+                          <p><span class="p-heading--five">imdb Rating:</span> {{movie.imdbRating}}</p>
                       </div>
                       <div class="col-6">
-                          <img src="https://via.placeholder.com/1024x325" alt="">
+                          <img class="movie-poster" :src="`${movie.Poster}`" alt="">
                       </div>
                   </div>
               </td>
